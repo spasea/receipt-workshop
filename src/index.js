5e72b0906ad97a4e5cf5f3f69255b75eddb13cc8
ithespv <ithespv@gmail.com>
2019-03-15 18:57:24 +0200

    Edit index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import Good from './Good'
 import Receipt from './Receipt'
+import ReceiptItem from './ReceiptItem'
 
 const apple = new Good(1, 10, 'apple')
 const potato = new Good(2, 12, 'potato')
 
+const applePosition = new ReceiptItem(apple, 1)
+const potatoPosition = new ReceiptItem(potato, 1)
+
 const receipt = new Receipt([
-  apple,
-  potato
+  applePosition,
+  potatoPosition
 ])
+
+console.log(receipt.totalPrice)
+
+receipt.addPosition(applePosition)
+receipt.addPosition(potatoPosition)
 
 console.log(receipt.totalPrice)
